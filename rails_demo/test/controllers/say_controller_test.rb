1c85bb262766f9f68efddb1f2f17810179ba2eb1
Jarez25 <jarez179@gmail.com>
2022-10-27 11:37:52 -0700

    mi primer aplicacion wed con ruby

---
+++ rails_demo/test/controllers/say_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class SayControllerTest < ActionDispatch::IntegrationTest
+  test "should get Hello" do
+    get say_Hello_url
+    assert_response :success
+  end
+
+  test "should get goodbye" do
+    get say_goodbye_url
+    assert_response :success
+  end
+end
